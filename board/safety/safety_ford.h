bc7f73c1da996d82fa2143fd694ec440f146bf44
Mrinmoy <Mrinmoy002@gmail.com>
2024-01-25 20:33:14 -0500

    force lcm2

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -147,7 +147,7 @@ const uint16_t FORD_PARAM_LONGITUDINAL = 1;
 const uint16_t FORD_PARAM_CANFD = 2;
 
 bool ford_longitudinal = false;
-bool ford_canfd = true;
+bool ford_canfd = false;
 
 const LongitudinalLimits FORD_LONG_LIMITS = {
   // acceleration cmd limits (used for brakes)

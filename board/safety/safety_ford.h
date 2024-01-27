a7c1de9035c00c6057568c9d24f851b047d2be33
Mrinmoy <Mrinmoy002@gmail.com>
2024-01-26 19:27:26 -0500

    force lcm2

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -41,7 +41,7 @@ const CanMsg FORD_CANFD_STOCK_TX_MSGS[] = {
   {FORD_Steering_Data_FD1, 2, 8},
   {FORD_ACCDATA_3, 0, 8},
   {FORD_Lane_Assist_Data1, 0, 8},
-  {FORD_LateralMotionControl2, 0, 8},
+  {FORD_LateralMotionControl2, 2, 8},
   {FORD_IPMA_Data, 0, 8},
 };
 
@@ -51,7 +51,7 @@ const CanMsg FORD_CANFD_LONG_TX_MSGS[] = {
   {FORD_ACCDATA, 0, 8},
   {FORD_ACCDATA_3, 0, 8},
   {FORD_Lane_Assist_Data1, 0, 8},
-  {FORD_LateralMotionControl2, 0, 8},
+  {FORD_LateralMotionControl2, 2, 8},
   {FORD_IPMA_Data, 0, 8},
 };
 
